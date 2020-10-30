ea14b79047ddfcddd44f9b9badd00fc616effde5
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2020-10-30 15:14:46 +0900

    Add rubocop-fjord

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,22 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i(github)
+         :omniauthable, omniauth_providers: %i[github]
 
   def self.create_unique_string
     SecureRandom.uuid
   end
 
-  def self.find_for_github_oauth(auth, signed_in_resource=nil)
+  def self.find_for_github_oauth(auth, _signed_in_resource = nil)
     user = User.find_by(provider: auth.provider, uid: auth.uid)
 
-    unless user
-      user = User.new(provider: auth.provider,
-                      uid:      auth.uid,
-                      name:     auth.info.name,
-                      email:    User.dummy_email(auth),
-                      password: Devise.friendly_token[0, 20]
-      )
-    end
+    user ||= User.new(provider: auth.provider,
+                      uid: auth.uid,
+                      name: auth.info.name,
+                      email: User.dummy_email(auth),
+                      password: Devise.friendly_token[0, 20])
     user.save
     user
   end
@@ -25,5 +24,4 @@ class User < ApplicationRecord
   def self.dummy_email(auth)
     "#{auth.uid}-#{auth.provider}@example.com"
   end
-
 end
