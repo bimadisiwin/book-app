8deb2c45f3395e9af875fb3ec2af89ae6411d413
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2020-11-05 23:43:09 +0900

    Improve: i18n for mailer

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_10_28_064425) do
     t.integer "zip_code"
     t.string "address"
     t.text "profile"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
