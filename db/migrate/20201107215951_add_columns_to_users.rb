1dec48059c43272a2525fb94d0e3f44fc235c057
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2020-11-09 08:19:26 +0900

    Fix: migration file(Add columns to users)

--- db/migrate/20201107215951_add_columns_to_users.rb
+++ db/migrate/20201107215951_add_columns_to_users.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
-    
-    add_column :users, :uid, :string, null: false
-    add_column :users, :provider, :string, null: false, default: ""
-    add_index :users, [:uid, :provider], unique: true
-    
+    add_column :users, :uid, :string, null: false, default: ''
+    add_column :users, :provider, :string, null: false, default: ''
+    add_index :users, %i[uid provider], unique: true
   end
 end
