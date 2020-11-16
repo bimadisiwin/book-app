8fb11f5104d86b57942727fcd2c1bb9b98884555
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2020-11-16 13:44:58 +0900

    Add: follow methods & controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,26 @@ class User < ApplicationRecord
   def self.dummy_email(auth)
     "#{auth.uid}-#{auth.provider}@example.com"
   end
+
+  has_many :relationships, foreign_key: "user_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :follow
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "follow_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :user
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+  
 end
