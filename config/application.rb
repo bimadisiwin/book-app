e9750df3172abc35d273c75b4d9fe4302c83a18f
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2020-11-25 08:33:11 +0900

    Change: Reduce the files generated by the generator

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,13 @@ module BooksApp
     config.load_defaults 6.0
 
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
