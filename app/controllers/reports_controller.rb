a8ffc372916637b01f168ac2ea01ead9f38662c7
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2020-11-26 10:17:21 +0900

    Remove: not use code about json

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,13 +2,11 @@ class ReportsController < ApplicationController
   before_action :set_report, only: [:show, :edit, :update, :destroy]
 
   # GET /reports
-  # GET /reports.json
   def index
     @reports = Report.all
   end
 
   # GET /reports/1
-  # GET /reports/1.json
   def show
   end
 
@@ -22,7 +20,6 @@ class ReportsController < ApplicationController
   end
 
   # POST /reports
-  # POST /reports.json
   def create
     @report = Report.new(report_params)
     @report.user_id = current_user.id
@@ -37,7 +34,6 @@ class ReportsController < ApplicationController
   end
 
   # PATCH/PUT /reports/1
-  # PATCH/PUT /reports/1.json
   def update
     respond_to do |format|
       if @report.update(report_params)
@@ -49,7 +45,6 @@ class ReportsController < ApplicationController
   end
 
   # DELETE /reports/1
-  # DELETE /reports/1.json
   def destroy
     @report.destroy
     respond_to do |format|
